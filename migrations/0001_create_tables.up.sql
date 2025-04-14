9d0123a0288f96d71205cb11ced55be41091459e
Wammero <107371364+Wammero@users.noreply.github.com>
2025-04-14 13:14:47 +0300

    indexes add

--- migrations/0001_create_tables.up.sql
+++ migrations/0001_create_tables.up.sql
@@ -49,3 +49,17 @@ CREATE TABLE IF NOT EXISTS reception_products (
     FOREIGN KEY (reception_id) REFERENCES receptions(reception_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
+
+CREATE INDEX IF NOT EXISTS idx_receptions_pvz_id ON receptions (pvz_id);
+CREATE INDEX IF NOT EXISTS idx_receptions_reception_time ON receptions (reception_time);
+CREATE INDEX IF NOT EXISTS idx_receptions_status ON receptions (status);
+CREATE INDEX IF NOT EXISTS idx_reception_products_product_id ON reception_products (product_id);
+CREATE INDEX IF NOT EXISTS idx_reception_products_reception_id ON reception_products (reception_id);
+CREATE INDEX IF NOT EXISTS idx_products_product_id ON products (product_id);
+CREATE INDEX IF NOT EXISTS idx_products_is_active ON products (is_active);
+CREATE INDEX IF NOT EXISTS idx_pvz_pvz_id ON pvz (id);
+CREATE INDEX IF NOT EXISTS idx_pvz_registration_date ON pvz (registration_date);
+CREATE INDEX IF NOT EXISTS idx_reception_products_is_active ON reception_products (is_active);
+CREATE INDEX IF NOT EXISTS idx_receptions_pvz_reception_time ON receptions (pvz_id, reception_time);
+CREATE INDEX IF NOT EXISTS idx_reception_products_product_id_is_active ON reception_products (product_id, is_active);
+CREATE INDEX IF NOT EXISTS idx_products_product_id_is_active ON products (product_id, is_active);
